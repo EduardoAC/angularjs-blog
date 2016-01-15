d3be8da526c4bc0412688f4e143f66ef74038fd3
EduardoAC <eduardo@dreammakerfactory.com>
2016-01-15 20:47:06 +0000

    Initialize angularJS blog based in Yeoman - Technologies Grunt + AngularJS + Yeoman + Bower

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularjsBlogApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularjsBlogApp
+ */
+angular.module('angularjsBlogApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularjsBlogApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularjsBlogApp
+ */
+angular.module('angularjsBlogApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'SitePoint'
+    ];
+  });
