1d88d78115e84f77da0cf4de55c573a186c95edb
EduardoAC <eduardo@dreammakerfactory.com>
2016-01-22 18:55:14 +0000

    Add post template for post grid layout - Add post template and fields - Add basic split styles based in left and right - Add directive with link class to customize post tag

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,23 @@
  * Controller of the angularjsBlogApp
  */
 angular.module('angularjsBlogApp')
-  .controller('MainCtrl', function ($scope) {
+.controller('MainCtrl', function ($scope) {
     this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma',
-      'SitePoint'
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma',
+        'SitePoint'
     ];
     $scope.date = new Date();
-  });
+}).directive('post', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            postInfo: '='
+        },
+        link: function ($scope, element, attrs) {
+            element.addClass("post");
+        },
+        templateUrl: 'templates/blog-post.html'
+    };
+});
