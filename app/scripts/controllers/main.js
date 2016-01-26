08af21c366aea743162e5d068279d6582f59b90e
EduardoAC <eduardo@dreammakerfactory.com>
2016-01-26 07:39:53 +0000

    Create first version of blog timeline - Responsive design styling - Add template system and directive to create a object - Add test file to emulate json comunication with the server

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the angularjsBlogApp
  */
 angular.module('angularjsBlogApp')
-.controller('MainCtrl', function ($scope) {
+.controller('MainCtrl', function ($scope, $http) {
     this.awesomeThings = [
         'HTML5 Boilerplate',
         'AngularJS',
@@ -16,15 +16,51 @@ angular.module('angularjsBlogApp')
         'SitePoint'
     ];
     $scope.date = new Date();
+    $scope.url = 'test/posts.json';
+    $scope.content = [];
+
+    $scope.fetchContent = function() {
+        $http.get($scope.url).then(function(result){
+            var even = false;
+            var oddPostList = [];
+            var evenPostList = [];
+            angular.forEach(result.data, function(val,ky){
+                if(even){
+                    evenPostList.push(val);
+                }else{
+                    oddPostList.push(val);
+                }
+                even = !even;
+            });
+            console.log(oddPostList);
+            $scope.oddPostList = oddPostList;
+            $scope.evenPostList = evenPostList;
+        });
+    };
+
+    $scope.fetchContent();
 }).directive('post', function () {
+    var linker = function ($scope, element, attrs) {
+        element.addClass("post");
+        console.log($scope);
+//        if(typeof $scope.postInfo === "undefined"){
+//            $scope.postInfo ={
+//                image: {
+//                    url: "http://eduardo-aparicio-cardenes.website/images/frontend/frameworks-libraries-plugins.jpg",
+//                    alt: "Standard Post with Image"
+//                },
+//                title: "Post title"
+//            }
+//        }
+        $scope.postInfo = $scope.$parent.postInfo;
+    };
+
     return {
         restrict: 'E',
         scope: {
             postInfo: '='
         },
-        link: function ($scope, element, attrs) {
-            element.addClass("post");
-        },
+        link: linker,
         templateUrl: 'templates/blog-post.html'
     };
 });
